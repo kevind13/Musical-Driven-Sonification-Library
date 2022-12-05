5ab5d3458c454e69ce833f3005d31ad567f5e674
Kevin <kevin@preki.com>
2022-12-05 14:17:10 +0900

    feat[OTH]: add list_files utils function

---
+++ utils/list_files.py
@@ -0,0 +1,15 @@
+import os
+
+def list_of_files(dir_name):
+    '''
+        For the given path, get the List of all files in the directory tree 
+    '''
+    list_of_file = os.listdir(dir_name)
+    all_files = list()
+    for entry in list_of_file:
+        full_path = os.path.join(dir_name, entry)
+        if os.path.isdir(full_path):
+            all_files = all_files + list_of_files(full_path)
+        else:
+            all_files.append(full_path)
+    return all_files
