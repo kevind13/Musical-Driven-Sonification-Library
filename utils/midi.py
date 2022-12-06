8315a800487d07ddca2364f6340d04f1e6a4e129
Kevin <kevin@preki.com>
2022-12-06 15:15:20 +0900

    feat: added new function to get tempo from MIDI file

---
+++ utils/midi.py
@@ -0,0 +1,7 @@
+def get_tempo(mid):
+    for track in mid.tracks:
+        for msg in track:
+            if msg.type == 'set_tempo':
+                return msg.tempo
+    else:
+        return 500000
