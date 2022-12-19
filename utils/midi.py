759faea9c567e5c2268c5a94b2914fa1350e1976
Kevin <kevin@preki.com>
2022-12-19 17:08:38 +0900

    fix: fix format of midi.py file

--- utils/midi.py
+++ utils/midi.py
@@ -4,6 +4,7 @@ import math
 from utils.list_files import list_of_files, list_of_files_no_depth
 from utils.statistics import draw_histogram, find_max_non_outlier
 
+
 def get_tempo(mid):
     for track in mid.tracks:
         for msg in track:
@@ -20,12 +21,13 @@ def cosecutive_on_off(midi_path):
     for index, track in enumerate(mid.tracks):
         total_len = len(track)
         for i in range(1, total_len):
-            if 'note_on' in str(track[i]) and i < total_len and 'note_off' not in str(track[i+1]):
+            if 'note_on' in str(track[i]) and i < total_len and 'note_off' not in str(track[i + 1]):
                 print(midi_path, index)
-                print(str(track[i-10: i+5]))
-                print(str(track[i]) == str(track[i+1]))
+                print(str(track[i - 10:i + 5]))
+                print(str(track[i]) == str(track[i + 1]))
                 return False
     return True
+
 
 def gcd_and_min_delta(midi_path):
     import math
@@ -36,30 +38,35 @@ def gcd_and_min_delta(midi_path):
     for track in mid.tracks:
         gcd_val = float('inf')
         min_delta = float('inf')
-        
+
         for i, event in enumerate(track):
-            if i == 0: continue
+            if i == 0:
+                continue
             delta = event.time
-            if gcd_val == float('inf'): gcd_val = delta
-            if delta == 0: continue
+            if gcd_val == float('inf'):
+                gcd_val = delta
+            if delta == 0:
+                continue
             gcd_val = math.gcd(gcd_val, delta)
             min_delta = min(min_delta, delta)
-        
+
         gcd_and_min_delta_values[track.name] = [gcd_val, min_delta]
-    
+
     list_of_gcd, list_of_min_deltas = zip(*gcd_and_min_delta_values.values())
     return math.gcd(*list_of_gcd), min(list_of_min_deltas)
+
 
 def gcd_from_list_of_midi(midi_directory):
     list_of_paths = list_of_files_no_depth(midi_directory)
     list_of_gcd = []
     for midi_file in list_of_paths:
-        try: 
+        try:
             temp_gcd, _ = gcd_and_min_delta(midi_file)
             list_of_gcd.append(temp_gcd)
         except:
             print(midi_file)
     return math.gcd(*list_of_gcd)
+
 
 def len_histogram_of_midi(midi_directory):
     list_of_paths = list_of_files_no_depth(midi_directory)
@@ -67,7 +74,7 @@ def len_histogram_of_midi(midi_directory):
     list_of_lenghts = []
     max_file = ''
     for midi_path in list_of_paths:
-        try: 
+        try:
             mid = mido.MidiFile(midi_path)
             tmp_max_len = float('-inf')
             for track in mid.tracks:
@@ -75,13 +82,20 @@ def len_histogram_of_midi(midi_directory):
                 for i in range(1, len(track)):
                     if 'note_on' in str(track[i]) or 'note_off' in str(track[i]):
                         tmp_len += track[i].time
-                tmp_max_len=max(tmp_max_len, tmp_len)
-                
+                tmp_max_len = max(tmp_max_len, tmp_len)
+
             list_of_lenghts.append((tmp_max_len, midi_path))
         except Exception as e:
             print(midi_path, e)
-    
+
     draw_histogram([x[0] for x in list_of_lenghts])
     max_non_outlier = find_max_non_outlier([x[0] for x in list_of_lenghts])
     max_len, max_file = max(list_of_lenghts)
-    return(max_len, max_file, max_non_outlier)
+    min_len, min_file = min(list_of_lenghts)
+    return ({
+        'max_len': max_len,
+        'max_file': max_file,
+        'max_non_outlier': max_non_outlier,
+        'min_len': min_len,
+        'min_file': min_file
+    })
