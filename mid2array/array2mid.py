aa449997652296b47ea1ed82ae994f1f0b7841c5
Kevin <kevin@preki.com>
2022-12-20 17:05:13 +0900

    refactor: added continue statement in array2mid function to avoid unnecessary conditional

--- mid2array/array2mid.py
+++ mid2array/array2mid.py
@@ -21,18 +21,19 @@ def array2mid(ary, tempo=None):
     for ch in changes:
         if set(ch) == {0}:  # no change
             last_time += 1
-        else:
-            on_notes = np.where(ch > 0)[0]
-            on_notes_vol = ch[on_notes]
-            off_notes = np.where(ch < 0)[0]
-            first_ = True
-            for n, v in zip(on_notes, on_notes_vol):
-                new_time = last_time if first_ else 0
-                track.append(mido.Message('note_on', note=n + 21, velocity=v, time=new_time))
-                first_ = False
-            for n in off_notes:
-                new_time = last_time if first_ else 0
-                track.append(mido.Message('note_off', note=n + 21, velocity=0, time=new_time))
-                first_ = False
-            last_time = 0
+            continue
+
+        on_notes = np.where(ch > 0)[0]
+        on_notes_vol = ch[on_notes]
+        off_notes = np.where(ch < 0)[0]
+        first_ = True
+        for n, v in zip(on_notes, on_notes_vol):
+            new_time = last_time if first_ else 0
+            track.append(mido.Message('note_on', note=n + 21, velocity=v, time=new_time))
+            first_ = False
+        for n in off_notes:
+            new_time = last_time if first_ else 0
+            track.append(mido.Message('note_off', note=n + 21, velocity=0, time=new_time))
+            first_ = False
+        last_time = 0
     return mid_new
