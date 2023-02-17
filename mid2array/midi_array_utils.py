e8d9f8582fb298935ffbdab0925f2ae13c17c96b
Kevin <kevin@preki.com>
2023-02-17 15:56:09 +0900

    feat: add function to compare midis

--- mid2array/midi_array_utils.py
+++ mid2array/midi_array_utils.py
@@ -11,7 +11,11 @@ def remove_ending_zeros(arr):
     return arr[:index + 1]
 
 
-def plot_midi_array(array, title: Optional[str] = None, x_label: Optional[str] = None, y_label: Optional[str] = None, legend:Optional[bool]=None):
+def plot_midi_array(array,
+                    title: Optional[str] = None,
+                    x_label: Optional[str] = None,
+                    y_label: Optional[str] = None,
+                    legend: Optional[bool] = None):
     fig = plt.figure()
 
     colors = ['b', 'g', 'orange', 'r']
@@ -29,4 +33,35 @@ def plot_midi_array(array, title: Optional[str] = None, x_label: Optional[str] =
         plt.title(title)
     if legend:
         plt.legend()
+    plt.show()
+
+
+def compare_midi_arrays(real,
+                        reconstructed,
+                        titles = None,
+                        x_label: Optional[str] = None,
+                        y_label: Optional[str] = None,
+                        legend: Optional[bool] = None):
+
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(12, 8))
+
+    colors = ['b', 'g', 'orange', 'r']
+    
+    for index, data in enumerate([real, reconstructed]):
+        for i in range(data.shape[0]):
+            notes = np.multiply(np.where(data[i] > 0, 1, 0), range(1, MIDI_NOTE_RANGE + 1)).max(axis=1)
+            notes = remove_ending_zeros(notes)
+            axs[index].plot(range(notes.shape[0]), notes, marker='_', linestyle='', color=colors[i], label=f'Track {i+1}')
+
+        if x_label is not None:
+            axs[index].set_xlabel(x_label)
+        if y_label is not None:
+            axs[index].set_ylabel(y_label)
+        if titles is not None:
+            axs[index].set_title(titles[index])
+        if legend:
+            axs[index].legend()
+
+
+    plt.tight_layout()
     plt.show()
