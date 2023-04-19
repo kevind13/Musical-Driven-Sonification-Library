f53acf69627613dd31a81505e66c1f7593615a73
Kevin <kevin@preki.com>
2023-04-19 23:43:16 +0900

    feat: remove groups from plotting functions

--- SAE-IBS/plot_functions.py
+++ SAE-IBS/plot_functions.py
@@ -19,21 +19,17 @@ def plot_loss(train_loss, val_loss, path, title=None):
     plt.show()
 
 
-def plot_latent(latent, cdict, group, figure_title, figure_name, path, run_parameters):
+def plot_latent(latent, figure_title, figure_name, path, run_parameters):
     ncol = latent.shape[1]
     if ncol == 2:
         fig, ax = plt.subplots()
-        for g in np.unique(group):
-            ix = np.where(group == g)[0].astype(int)
-            ax.scatter(latent[ix, 0], latent[ix, 1], c=cdict[g], label=g, s=2)
+        ax.scatter(latent[:, 0], latent[:, 1], s=2)
         ax.legend()
     else:
         w, h = plt.figaspect(0.2)
         fig, ax = plt.subplots(1, int(ncol / 2), figsize=(w, h))
         for i, j in zip(range(0, ncol, 2), range(0, int(ncol / 2))):
-            for g in np.unique(group):
-                ix = np.where(group == g)
-                ax[j].scatter(latent[ix, i], latent[ix, i + 1], c=cdict[g], label=g, s=2)
+            ax[j].scatter(latent[:, i], latent[:, i + 1], s=2)
             #ax[j].legend()
             ax[j].set_xlabel('Axis' + str(i + 1), fontsize=10)
             ax[j].set_ylabel('Axis' + str(i + 2), fontsize=10)
