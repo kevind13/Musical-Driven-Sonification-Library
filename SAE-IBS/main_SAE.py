62be60df1576f1367e317cf8e2dbc188a18545d0
Kevin <kevin@preki.com>
2023-04-20 23:07:49 +0900

    feat: change name of file for SAE and fix some bugs

--- SAE-IBS/main_SAE.py
+++ SAE-IBS/main_SAE.py
@@ -9,7 +9,7 @@ import torch
 import numpy as np
 import pandas
 from functions import *
-from model import Autoencoder, SAEIBS
+from model import SAE, Autoencoder
 from plot_functions import plot_latent
 from arguments import parse_args, save_args
 from keras.utils.np_utils import to_categorical
@@ -30,6 +30,7 @@ cudnn.deterministic = True
 
 def main():
     args = parse_args()
+    print(args)
     args.work_dir = os.path.dirname(os.path.realpath(__file__))
     run_parameters = str(len(args.hidden_dim)) + '_Layer_' + str(args.latent_dim) + '_Latent_' + "_".join(str(x) for x in args.hidden_dim)
     args.out_dir = os.path.join(args.work_dir, 'results/' + args.model_name + '/' + args.model_name + '_' + run_parameters)
@@ -54,7 +55,12 @@ def main():
     # pre-train an AE model
     input_dim = X_train.shape[1:]
 
-    pre_model = Autoencoder(input_dim, args.hidden_dim[:-1], args.hidden_dim[-1], args.cond_dropout, args.drop_rate, args.actFn).to(device)
+    pre_model = Autoencoder(input_dim, args.hidden_dim[:-1], args.hidden_dim[-1], args.cond_dropout, args.drop_rate, args.actFn)
+
+    if args.init_weights:
+        pre_model.apply(init_weights)
+
+    pre_model = pre_model.to(device)
 
     num_params = sum(p.numel() for p in pre_model.parameters() if p.requires_grad)
     print('Number of parameters: %d' % num_params)
@@ -65,8 +71,9 @@ def main():
     pre_model, train_loss1, val_loss1, _, = run_AE(pre_model, X_train, X_test, args.batch_size, optimizer, scheduler, device,
                                                      args.pretrain_epochs, args.out_dir, args.patience, args.loss_opt)
 
-    # train SAE-IBS
-    model = SAEIBS(input_dim, args.hidden_dim, args.latent_dim, args.cond_dropout, args.drop_rate, args.actFn).to(device)
+
+    # train SAE
+    model = SAE(input_dim, args.hidden_dim, args.latent_dim, args.cond_dropout, args.drop_rate, args.actFn).to(device)
 
     update_dict = {}
     pre_model_dict = pre_model.state_dict()
@@ -87,33 +94,22 @@ def main():
     optimizer = torch.optim.Adam(params=model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.decay_step, gamma=args.lr_decay)
 
-    model, train_loss2, val_loss2, val_loss_min, V = run_SAEIBS(model, X_train, X_test, args.batch_size, optimizer, scheduler, device,
+    model, train_loss2, val_loss2, val_loss_min, V = run_SAE(model, X_train, X_test, args.batch_size, optimizer, scheduler, device,
                                                                 args.pretrain_epochs, args.out_dir, args.patience, loss_opt=args.loss_opt)
-    
-    assert False, 'Se supone que acá ya tengo el modelo guardadito'
 
     # Project
-    group = population
-    cdict = {'EUR': 'blue', 'EAS': 'red', 'AMR': 'yellow', 'SAS': 'purple', 'AFR': 'green'}
-    latent_1kg, mean_emb_1kg = projectSAEIBS_traindata(model, geno_1kg, ibs_1kg, V, args.batch_size, device)
-    plot_latent(latent_1kg, cdict, group, "1 KG " + args.model_name, "_1KG", args.out_dir, run_parameters)
-
-    group2 = np.append(population, np.array([['HDGP'] for _ in range(geno_hdgp_sub.shape[0])]))
-    cdict2 = {'EUR': 'blue', 'EAS': 'red', 'AMR': 'yellow', 'SAS': 'purple', 'AFR': 'green', 'HDGP': 'grey'}
-    latent_hdgp_sub = projectSAEIBS_newdata(model, geno_hdgp_sub, ibs_hdgp_sub, V, mean_emb_1kg, args.batch_size, device)
-    latent = np.concatenate((latent_1kg, latent_hdgp_sub), axis=0)
-    plot_latent(latent, cdict2, group2, "1KG HDGP; " + args.model_name, "_1KG_HDGP", args.out_dir, run_parameters)
+    X_project = np.array(random.choices(X_train, k=20))
+    X_pred, latent_project = projectSAE(model, X_project, args.batch_size, args.latent_dim, device)
+    
+    plot_latent(latent_project, "Test " + args.model_name, "_test", args.out_dir, run_parameters)
 
     latent_path = args.out_dir + '/latent'
     if not os.path.exists(latent_path):
         os.makedirs(latent_path)
 
-    # save 1KG latent
-    latent_1KG = latent_1kg
-    savemat(latent_path + '/latent_1KG.mat', mdict={'latent_1KG': latent_1KG})
-    # save HDGP-sub latent
-    savemat(latent_path + '/latent_hdgp.mat', mdict={'latent_hdgp': latent_hdgp_sub})
-
+    # save latent
+    savemat(latent_path + '/latent_project.mat', mdict={'latent_project': latent_project, 'test_data': X_project, 'pred_data': X_pred})
+    
     # Save Model
     path_model = args.out_dir + '/' + args.model_name + '_' + run_parameters + '.tar'
     torch.save({'model': model,
@@ -121,7 +117,7 @@ def main():
                 'V': V}, path_model)
 
     # Plot cov matrix
-    cov_emb = orthogonality(latent_1kg, args.out_dir, run_parameters, args.model_name + ' ')
+    cov_emb = orthogonality(latent_project, args.out_dir, run_parameters, args.model_name + ' ')
 
 
 if __name__ == '__main__':
